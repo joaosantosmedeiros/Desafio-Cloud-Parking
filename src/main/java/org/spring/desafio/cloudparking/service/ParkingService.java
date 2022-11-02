6e0f4a0c85983cfb1fe83d12a1114de1f7e86810
darthjotinha <jopesame@gmail.com>
2022-11-02 13:35:18 -0300

    Primeiro endpoint de lista

---
+++ src/main/java/org/spring/desafio/cloudparking/service/ParkingService.java
@@ -0,0 +1,28 @@
+package org.spring.desafio.cloudparking.service;
+
+import org.spring.desafio.cloudparking.model.Parking;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class ParkingService {
+
+    private static Map<String, Parking> parkingMap = new HashMap<>();
+
+    static {
+        var id = getUUID();
+        Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        parkingMap.put(id, parking);
+    }
+
+    public List<Parking> findAll(){
+        return parkingMap.values().stream().collect(Collectors.toList());
+    }
+
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+}

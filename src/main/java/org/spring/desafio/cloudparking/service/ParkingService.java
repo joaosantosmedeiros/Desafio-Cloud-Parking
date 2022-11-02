5fa10a903000ec6a0f5b1d08157d02c59dae4d60
darthjotinha <jopesame@gmail.com>
2022-11-02 17:13:37 -0300

    Adicionando Swagger

--- src/main/java/org/spring/desafio/cloudparking/service/ParkingService.java
+++ src/main/java/org/spring/desafio/cloudparking/service/ParkingService.java
@@ -3,6 +3,7 @@ package org.spring.desafio.cloudparking.service;
 import org.spring.desafio.cloudparking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,8 +14,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        Parking parking1 = new Parking(id1, "MST-1234", "SP", "VW GOL", "VERDE");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll(){
@@ -25,4 +29,15 @@ public class ParkingService {
         return UUID.randomUUID().toString().replace("-","");
     }
 
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
 }

5f8727f5eff6d0a6f8582c8561466b9149c8045e
darthjotinha <jopesame@gmail.com>
2022-11-03 09:41:10 -0300

    Add tests

--- src/test/java/org/spring/desafio/cloudparking/controller/ParkingControllerTest.java
+++ src/test/java/org/spring/desafio/cloudparking/controller/ParkingControllerTest.java
@@ -1,16 +1,56 @@
 package org.spring.desafio.cloudparking.controller;
 
+import io.restassured.RestAssured;
+import org.codehaus.groovy.control.io.ReaderSource;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.spring.desafio.cloudparking.dto.ParkingCreateDTO;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ParkingControllerTest {
 
-    @Test
-    void findAll() {
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest(){
+        System.out.println(randomPort);
+        RestAssured.port = randomPort;
     }
 
     @Test
-    void create() {
+    void whenFindAllThenCheckResult() {
+        RestAssured.given()
+                .when()
+                .get("/parking")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void whenCreateThenCheckIsCreated() {
+
+        var createDTO = new ParkingCreateDTO();
+        createDTO.setColor("Amarelo");
+        createDTO.setLicense("WRT-5555");
+        createDTO.setModel("BRASILIA");
+        createDTO.setState("RN");
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("/parking")
+                .then()
+                .statusCode(201)
+                .body("license", Matchers.equalTo("WRT-5555"))
+                .body("color", Matchers.equalTo("Amarelo"));
     }
 }
